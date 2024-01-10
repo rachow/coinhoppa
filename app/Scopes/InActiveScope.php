ccfa1843aec9a526d431f060600188ebe0f8e0cc
Rahel Chowdhury <rahelahmed79@gmail.com>
2024-01-10 23:46:14 +0000

    scopes allowing us to keep code lean and access ORMS based on these scopes.

---
+++ app/Scopes/InActiveScope.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ *	@author: $rachow
+ *	@copyright: Coinhoppa
+ *
+ *	For inactive scope in ORMs.
+ *
+*/
+
+namespace App\Scopes;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+
+class InActiveScope implements Scope
+{
+	/**
+     * Apply the scope to a given Eloquent query builder.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $builder
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @return void
+     */
+    public function apply(Builder $builder, Model $model)
+    {
+        $builder->where('active', '=', 0);
+    }
+}
