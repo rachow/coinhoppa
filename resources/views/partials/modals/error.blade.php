37cf90f45644bbf77c3838394c2941e79e84c317
Rahel Chowdhury <rahelahmed79@gmail.com>
2024-01-11 14:32:06 +0000

    more view and modals added.

---
+++ resources/views/partials/modals/error.blade.php
@@ -0,0 +1,13 @@
+<div class="modal fade" id="modal-error-alert" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-hidden="true">
+	<div class="modal-dialog modal-sm modal-dialog-centered">
+		<div class="modal-content">
+			<div class="modal-body text-center">
+				<h3 class="mb-15"><i class="fa fa-exclamation-triangle"></i> Error</h3>
+				<p class="modal-error-text pb-3" data-state="An error occurred, please contact operations team.">
+					An error occurred, please contact operations team.
+				</p>
+				<button type="button" class="btn btn-primary" data-dismiss="modal">OK</button>
+			</div>
+		</div>
+	</div>
+</div>
